82d0c005f3127bcf1cc5db040bde54a31fe3799f
arnoldknott <knott@dtu.dk>
2026-01-13 16:30:43 +0100

    ci: prepares ChatGMP for deployment in Azure

--- scripts/enter_chatgmp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-REPO_ROOT_DIR=$(git rev-parse --show-toplevel)
-cd $REPO_ROOT_DIR
-docker compose -f compose.yml -f compose.prod.yml build chatgmp
-docker compose -f compose.yml -f compose.prod.yml up chatgmp -d
-docker compose -f compose.yml -f compose.prod.yml exec chatgmp sh -c "/bin/sh"
-docker compose -f compose.yml -f compose.prod.yml down
